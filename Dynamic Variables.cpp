a8df5e3de6d96eb27668c8c6be1e1a6a7b0f94a5
Muhammad Shayan Anwar <155350566+Shayan-codingways@users.noreply.github.com>
2024-01-31 23:50:15 +0500

    Add files via upload

---
+++ Dynamic Variables.cpp
@@ -0,0 +1,7 @@
+/*
+Variables created during the program execution are called dynamic variables
+
+● To create a dynamic variable we use new operator. The new operator allocates the memory of a designated type.
+○ It returns a pointer to the allocated memory.
+
+*/
